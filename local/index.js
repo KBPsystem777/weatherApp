27563c56a43d7229ec3fea7def2dd45dac712d8c
kbpsystem777 <koleen.bp@outlook.com>
2018-05-25 05:16:14 +0800

    json.data.weather[0].description

--- local/index.js
+++ local/index.js
@@ -19,9 +19,7 @@ console.log(`Listening on http://localhost:${port}`);
 const url = 'https://fcc-weather-api.glitch.me/api/current?';
 
 let options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
+    enableHighAccuracy: true
 };
 
 function success(pos) {
@@ -29,14 +27,18 @@ function success(pos) {
     let latitude = crd.latitude;
     let longitude = crd.longitude;
     let endPoint = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+    
     axios.get(endPoint)
         .then(function (json) {
             console.log(endPoint)
-            console.log(latitude, longitude)
+            //console.log(json.data.weather[0].description);
+            let description = json.data.weather[0].description;
             document.getElementById('temp').innerHTML = json.data.main.temp + '&deg';
             document.getElementById('city').innerHTML = json.data.name;
             document.getElementById('country').innerHTML = json.data.sys.country;
-            document.getElementById('location').innerHTML = `${latitude}, ${longitude}`;
+            document.getElementById('description').innerHTML = json.data.weather[0].description;
+
+
         });
 };
 
