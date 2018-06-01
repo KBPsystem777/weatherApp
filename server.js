4efc4d3a207d5ff07d07aaddf1c6cd6a155734b3
kbpsystem777 <koleen.bp@outlook.com>
2018-06-01 20:53:36 +0800

    Serve Static files under Public folder

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+//Serve Static files
+app.use(express.static('public'));
+
 app.listen(port);
 
 console.log(`Listening to http://localhost:${port}`);
