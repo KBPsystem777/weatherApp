f043eef87edf0918cda43d9ec0afe4c6bf02eaf6
kbpsystem777 <koleen.bp@outlook.com>
2018-06-02 04:10:24 +0800

    Show local time

--- public/index.js
+++ public/index.js
@@ -1,51 +1,52 @@
-//import axios from '../node_modules/axios';
-/*
-const http = require('http');
-const fs = require('fs');
 
-const page = fs.readFileSync('index.html');
 
-const port = 8080;
-
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(page);
-}).listen(port);
-console.log(`Listening on http://localhost:${port}`);
-*/
-
-//Geolocation
-
-const url = 'https://fcc-weather-api.glitch.me/api/current?';
-
-let options = {
-    enableHighAccuracy: true
-};
-
-function success(pos) {
-    let crd = pos.coords;
-    let latitude = crd.latitude;
-    let longitude = crd.longitude;
-    let endPoint = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+    function dataRun() {
     
-    axios.get(endPoint)
-        .then(function (json) {
-            console.log(endPoint)
-            //console.log(json.data.weather[0].description);
-            let description = json.data.weather[0].description;
-            document.getElementById('temp').innerHTML = json.data.main.temp + '&deg';
-            document.getElementById('city').innerHTML = json.data.name;
-            document.getElementById('country').innerHTML = json.data.sys.country;
-            document.getElementById('description').innerHTML = json.data.weather[0].description;
-        });
-};
+        const url = 'https://fcc-weather-api.glitch.me/api/current?';
 
-function error(err) {
-    console.log(`ERROR(${err.code}): ${err.message}`);
-};
+        let options = {
+            enableHighAccuracy: true
+        };
 
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-}
+        function success(pos) {
+            let crd = pos.coords;
+            let latitude = crd.latitude;
+            let longitude = crd.longitude;
+            let endPoint = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+
+            axios.get(endPoint)
+                .then(function (json) {
+                    //console.log(endPoint)
+                    let description = json.data.weather[0].description;
+                    document.getElementById('temp').innerHTML = json.data.main.temp + '&deg';
+                    document.getElementById('city').innerHTML = json.data.name + ', ' + json.data.sys.country
+                    document.getElementById('description').innerHTML = json.data.weather[0].main;
+                });
+        };
+
+        function error(err) {
+            console.log(`ERROR(${err.code}): ${err.message}`);
+        };
+            
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(success, error, options);
+        }
+    };
 
 
+    setTimeout(
+        function startTime() {
+        let today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('clock').innerHTML = `Local Time: ${h}:${m}:${s}`;
+        let t = setTimeout(startTime, 500);
+    }, 2000);
+
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+        return i;
+    };
