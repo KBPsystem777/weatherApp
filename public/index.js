17e8e9931090e5d24f2994deac0ce97c6e28ff61
kbpsystem777 <koleen.bp@outlook.com>
2018-06-13 22:31:03 +0800

    celsius to F converter function

--- public/index.js
+++ public/index.js
@@ -1,28 +1,27 @@
-
-
-    function dataRun() {
-    
         const url = 'https://fcc-weather-api.glitch.me/api/current?';
-
+        
+        let crd = pos.coords;
+        let latitude = crd.latitude;
+        let longitude = crd.longitude;
+        let endPoint = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+        console.log(`User Location is Latitude: ${latitude}, Longitude: ${longitude}`);
+        
         let options = {
             enableHighAccuracy: false
         };
 
         function success(pos) {
-            let crd = pos.coords;
-            let latitude = crd.latitude;
-            let longitude = crd.longitude;
-            let endPoint = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
-            console.log(`User Location is Latitude: ${latitude}, Longitude: ${longitude}`);
             axios.get(endPoint)
                 .then(function (json) {
                     //console.log(endPoint)
                     let description = json.data.weather[0].description;
-                    document.getElementById('temp').innerHTML = json.data.main.temp + '&deg';
+                    let temp = json.data.main.temp;
+                    document.getElementById('temp').innerHTML = `${temp}&deg`;
                     document.getElementById('city').innerHTML = json.data.name + ', ' + json.data.sys.country
                     document.getElementById('description').innerHTML = json.data.weather[0].main;
-                });
+            });
         };
+
 
         function error(err) {
             console.log(`ERROR(${err.code}): ${err.message}`);
@@ -31,22 +30,34 @@
         if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition(success, error, options);
         }
-    };
 
 
-    setTimeout(
-        function startTime() {
-        let today = new Date();
-        let h = today.getHours();
-        let m = today.getMinutes();
-        let s = today.getSeconds();
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('clock').innerHTML = `Local Time: ${h}:${m}:${s}`;
-        let t = setTimeout(startTime, 500);
-    }, 2000);
+        setTimeout(
+            function startTime() {
+            let today = new Date();
+            let h = today.getHours();
+            let m = today.getMinutes();
+            let s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('clock').innerHTML = `Local Time: ${h}:${m}:${s}`;
+            let t = setTimeout(startTime, 500);
+        }, 2000);
 
-    function checkTime(i) {
-        if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-        return i;
-    };
+        function checkTime(i) {
+            if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+            return i;
+        };
+
+        function convertToF() {
+            axios.get(endPoint)
+                .then(function (json) {
+                    //console.log(endPoint)
+                    let description = json.data.weather[0].description;
+                    let temp = (json.data.main.temp * 9) / 5 + 32;
+                    document.getElementById('temp').innerHTML = `${temp}&deg`;
+                    document.getElementById('city').innerHTML = json.data.name + ', ' + json.data.sys.country
+                    document.getElementById('description').innerHTML = json.data.weather[0].main;
+                });
+            document.getElementById('temp').innerHTML = temp;
+        };
