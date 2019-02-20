371c09f7526e94637ad25bfe3acd1e056da4f3c1
kbpsystem777 <koleen.bp@outlook.com>
2019-02-21 06:41:02 +0800

    building temp and app display

--- reactified-weather-app/src/App.js
+++ reactified-weather-app/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import Header from './Components/Header'
 import './App.css';
+
+import Header from './Components/Header';
+import TemperatureDisplay from './Components/TemperatureDisplay';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TemperatureDisplay />
       </div>
     );
   }
